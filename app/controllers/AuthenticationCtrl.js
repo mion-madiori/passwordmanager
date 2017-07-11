97d3f4ec1bdad68e457b9dd489710edb01425787
Albert Yoann <albert.yoann@free.fr>
2017-07-11 09:28:54 +0200

    Add methode in AuthenticationCtrl in app/controllers

--- app/controllers/AuthenticationCtrl.js
+++ app/controllers/AuthenticationCtrl.js
@@ -2,6 +2,19 @@ const _ = require('underscore');
 
 class AuthenticationCtrl {
 
+    constructor(data, userService) {
+
+    }
+
+    loginAction(req, res) {
+        res.render('', this._data)
+    }
+
+    login(req, res) {
+        res.render('', {
+            message: ''
+        });
+    }
 }
 
 module.exports = AuthenticationCtrl;
