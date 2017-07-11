9a225f3382e4fb2c533a5f05ec027cefd674f3a9
vincent <vincent.cr@outlook.fr>
2017-07-11 13:50:58 +0200

    implements 4 of 8 tests

--- app/controllers/AuthenticationCtrl.js
+++ app/controllers/AuthenticationCtrl.js
@@ -3,15 +3,31 @@ const _ = require('underscore');
 class AuthenticationCtrl {
 
     constructor(data, userService) {
-
+        this._data = data;
+        this._userService = userService;
     }
 
     login(req, res) {
-        res.render('', this._data)
+        res.render('authentication/login', this._data)
     }
 
     postlogin(req, res) {
-        res.render('', {
+
+        if(req.body.constructor === Object && Object.keys(req.body).length === 0 ){
+            res.render('authentication/login', {
+                message: 'All fields required'
+            })
+            return
+        }
+
+        if(req.body.password === ''){
+            res.render('authentication/login', {
+                message: 'All fields required'
+            })
+            return
+        }
+
+        res.render('authentication/login', {
             message: ''
         });
     }
