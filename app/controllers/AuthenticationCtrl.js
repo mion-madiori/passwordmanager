1e392562ff2ee57ed4fb3abaf32e51f19cba9d89
Albert Yoann <albert.yoann@free.fr>
2017-07-11 14:46:52 +0200

    Refacto methode postLogin in AuthenticationCtrl (app)

--- app/controllers/AuthenticationCtrl.js
+++ app/controllers/AuthenticationCtrl.js
@@ -7,31 +7,32 @@ class AuthenticationCtrl {
         this._userService = userService;
     }
 
-    forgotPassword(req, res){
+    forgotPassword(req, res) {
         res.redirect('/resetPassword', this._data);
     }
 
-    resetPassword(req, res){
-        if(_.isEmpty(req.body)) {
+    resetPassword(req, res) {
+        if (_.isEmpty(req.body)) {
             res.render('/resetPassword', {
                 message: 'Email address required'
             });
             return;
         }
-        if(req.body.email === 'test-email@gmail.com') {
+        if (req.body.email === 'test-email@gmail.com') {
             res.render('/resetPassword', {
-               password: 'p@ssword'
+                password: 'p@ssword'
             });
             return;
         }
     }
 
-    emailExistInBdd(req, res){
-        if(req.body.email === 'incorrect-email@gmail.com') {
+    emailExistInBdd(req, res) {
+        if (req.body.email === 'incorrect-email@gmail.com') {
             res.render('/resetPassword', {
                 message: 'Incorrect email address'
             });
         }
+    }
 
     login(req, res) {
         res.render('authentication/login', this._data)
@@ -39,23 +40,19 @@ class AuthenticationCtrl {
 
     postlogin(req, res) {
 
-        if(req.body.constructor === Object && Object.keys(req.body).length === 0 ){
+        if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
             res.render('authentication/login', {
                 message: 'All fields required'
-            })
+            });
             return
         }
 
-        if(req.body.password === ''){
+        if (req.body.password === '') {
             res.render('authentication/login', {
                 message: 'All fields required'
-            })
+            });
             return
         }
-
-        res.render('authentication/login', {
-            message: ''
-        });
     }
 }
 
