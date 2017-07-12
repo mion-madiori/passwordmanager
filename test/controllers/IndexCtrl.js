c0f4ee845febad4662fdeb38a17a85055263a674
vincent <vincent.cr@outlook.fr>
2017-07-12 11:09:39 +0200

    add 'addPassword' method in UserModel class

--- test/controllers/IndexCtrl.js
+++ test/controllers/IndexCtrl.js
@@ -1,3 +1,16 @@
 /**
  * Created by Marie on 10/07/2017.
+ * Use case:
+ * 
  */
+
+const expect = require('expect');
+const IndexCtrl = require('../../app/controllers/IndexCtrl');
+
+describe('IndexCtrl', () => {
+    describe('#getRoute', () => {
+        it('', () => {
+            
+        });
+    });
+});
